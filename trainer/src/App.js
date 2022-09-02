114c644bcb0143a64545ccdc71e36e5b7312b343
SebastianCCC <89015968+SebastianCCC@users.noreply.github.com>
2022-09-02 10:40:39 +0200

    add routing

--- trainer/src/App.js
+++ trainer/src/App.js
@@ -1,5 +1,22 @@
+import { Route, Routes } from 'react-router-dom'
+import ClassDetail from './Pages/ClassDetail'
+import Home from './Pages/Home'
+import Schedule from './Pages/Schedule'
+import Search from './Pages/Search'
+import Welcome from './Pages/Welcome'
+
 function App() {
-  return <h1>heyy</h1>
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="home" element={<Home />} />
+        <Route path="search" element={<Search />} />
+        <Route path="schedule" element={<Schedule />} />
+        <Route path="schedule/:id" element={<ClassDetail />} />
+      </Routes>
+    </div>
+  )
 }
 
 export default App
---
+++ trainer/src/Pages/ClassDetail.js
@@ -0,0 +1,5 @@
+const ClassDetail = () => {
+  return <h1>ClassDetail</h1>
+}
+
+export default ClassDetail
