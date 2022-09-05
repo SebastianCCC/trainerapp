012ab22f01bfe17181fe8d01d0fccc4f34f11225
SebastianCCC <89015968+SebastianCCC@users.noreply.github.com>
2022-09-05 12:10:50 +0200

    add list of classes, initial commit

--- trainer/src/Pages/Home.js
+++ trainer/src/Pages/Home.js
@@ -1,5 +1,28 @@
+import ClassCard from '../Components/Main/ClassCard'
+import TrainerApi from '../Hooks/TrainerApiClass'
+
+const array = [
+  {
+    title: 'hello',
+  },
+  {
+    title: 'hello again',
+  },
+]
+
 const Home = () => {
-  return <h1>Home</h1>
+  const { classdata } = TrainerApi({})
+  return (
+    <div className="p-page">
+      <h2 className="font-bold text-2xl">Classes for you</h2>
+      <section className="flex overflow-auto py-4">
+        {classdata &&
+          classdata.map(({ className, asset, id }, i) => {
+            return <ClassCard key={i} title={className} img={asset.url} id={id} />
+          })}
+      </section>
+    </div>
+  )
 }
 
 export default Home
