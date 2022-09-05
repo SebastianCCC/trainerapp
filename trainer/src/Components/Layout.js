d3e19eef7c47d70ff60da7617d1f28bcfe10d1eb
SebastianCCC <89015968+SebastianCCC@users.noreply.github.com>
2022-09-05 15:13:03 +0200

    add Header, initial commit

--- trainer/src/Components/Layout.js
+++ trainer/src/Components/Layout.js
@@ -1,7 +1,15 @@
+import { useLocation } from 'react-router-dom'
+import NavTop from './Header/index'
+
 const Layout = ({ children }) => {
+  let { pathname } = useLocation()
+  const title = pathname.replace('/', '').replace('home', 'popular classes')
+  const displayTitle = !title.includes('class/')
   return (
     <div className="max-w-mobile">
-      <header></header>
+      <header className="sticky top-0">
+        {title && <NavTop title={title} displayTitle={displayTitle} />}
+      </header>
       <main>{children}</main>
     </div>
   )
