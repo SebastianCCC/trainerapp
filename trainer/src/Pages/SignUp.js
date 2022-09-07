1860907cb499376e0821fda6357828d897fb2a19
SebastianCCC <89015968+SebastianCCC@users.noreply.github.com>
2022-09-07 13:43:01 +0200

    add validation on login form

--- trainer/src/Pages/SignUp.js
+++ trainer/src/Pages/SignUp.js
@@ -1,4 +1,21 @@
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useForm } from 'react-hook-form'
+import { schema } from '../schema/LoginSchema'
+
 const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  })
+
+  const onSubmit = (data) => {
+    reset()
+    console.log(data)
+  }
   return (
     <>
       <div className="flex flex-col justify-end row-start-1 row-end-2 col-span-full z-10">
@@ -11,22 +28,37 @@ const SignUp = () => {
         </div>
       </div>
       <div className="p-page">
-        <form className="flex flex-col w-full">
+        <form className="flex flex-col w-full" onSubmit={handleSubmit(onSubmit)}>
           <label className="font-semibold pt-3">Log in with your credentials</label>
-          <div className="pl-5 my-4 bg-additional border border-ternary rounded-full w-full">
+          <div
+            className={`pl-5 my-4 bg-additional border ${
+              errors.email ? 'border-red-800' : 'border-ternary'
+            } rounded-full w-full`}
+          >
             <input
               className="placeholder:text-secondary w-full p-3 bg-transparent outline-none"
               placeholder="Enter your email..."
               type="text"
+              {...register('email')}
             />
           </div>
-          <div className="pl-5 mb-4 bg-additional border border-ternary rounded-full w-full">
+          <div
+            className={`pl-5 mb-4 bg-additional border ${
+              errors.password ? 'border-red-800' : 'border-ternary'
+            } rounded-full w-full`}
+          >
             <input
               className="placeholder:text-secondary w-full p-3 bg-transparent outline-none"
               placeholder="Enter your password..."
               type="password"
+              {...register('password')}
             />
           </div>
+          {Boolean(Object.keys(errors).length) && (
+            <p className="w-full select-none mb-4">
+              {errors.email?.message || errors.password?.message}
+            </p>
+          )}
           <button className="bg-primary py-[12px] px-8 w-full font-semibold text-base tracking-wide uppercase rounded-full">
             log in
           </button>
